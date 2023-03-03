8a822696905951c3585036e60a95ce2ad8343297
Harpreet Somel <somel.harpreet@utexas.edu>
2023-03-03 13:08:49 -0800

    removed header html

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1>hello world</h1>
-      </header>
+      <h1>hello world</h1>
     </div>
   );
 }
